6c64663fde89c625beadb3d162db3a51e7444ca9
Sava <sava.vuckovic@quantox.com>
2019-02-14 21:09:25 +0100

    added home page separate from other routes and added app styling

--- src/app/App.js
+++ src/app/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Container } from 'semantic-ui-react';
-import { Route } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 import NavBar from '../nav/NavBar';
 import HomePage from '../home/HomePage';
 import EventDashboard from '../events/EventDashboard';
@@ -13,18 +13,30 @@ import EventForm from '../events/EventForm';
 class App extends React.Component {
   render() {
     return (
-      <React.Fragment>
-        <NavBar />
-        <Container>
+      <>
+        <Switch>
           <Route exact path="/" component={HomePage} />
-          <Route path="/events" component={EventDashboard} />
-          <Route path="/event/:id" component={EventDetailsPage} />
-          <Route path="/people" component={PeopleDashboard} />
-          <Route path="/profile/:id" component={UserDetailsPage} />
-          <Route path="/settings" component={SettingsDashboard} />
-          <Route path="/createEvent" component={EventForm} />
-        </Container>
-      </React.Fragment>
+        </Switch>
+
+        <Route
+          path="/(.+)"
+          render={() => (
+            <>
+              <NavBar />
+              <Container>
+                <Switch>
+                  <Route path="/events" component={EventDashboard} />
+                  <Route path="/event/:id" component={EventDetailsPage} />
+                  <Route path="/people" component={PeopleDashboard} />
+                  <Route path="/profile/:id" component={UserDetailsPage} />
+                  <Route path="/settings" component={SettingsDashboard} />
+                  <Route path="/createEvent" component={EventForm} />
+                </Switch>
+              </Container>
+            </>
+          )}
+        />
+      </>
     );
   }
 }
